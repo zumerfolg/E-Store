97e873eb21ac5031dc6e38359e4cde0444cd0edd
Daniel Choi <zumerfolg@gmail.com>
2013-04-13 11:43:39 -0500

    admin form tag

--- robovacuum/app/controllers/orders_controller.rb
+++ robovacuum/app/controllers/orders_controller.rb
@@ -2,17 +2,7 @@ class OrdersController < ApplicationController
   
   before_filter :get_cart, :only => [:new, :create]
   
-  # GET /orders
-  # GET /orders.json
-  def index
-    @orders = Order.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @orders }
-    end
-  end
-
+ 
   # GET /orders/1
   # GET /orders/1.json
   def show
@@ -86,10 +76,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  # GET /orders/1/edit
-  def edit
-    @order = Order.find(params[:id])
-  end
 
   # POST /orders
   # POST /orders.json
@@ -136,33 +122,6 @@ class OrdersController < ApplicationController
     end
   end
 
-  # PUT /orders/1
-  # PUT /orders/1.json
-  def update
-    @order = Order.find(params[:id])
-
-    respond_to do |format|
-      if @order.update_attributes(params[:order])
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order = Order.find(params[:id])
-    @order.destroy
-
-    respond_to do |format|
-      format.html { redirect_to orders_url }
-      format.json { head :no_content }
-    end
-  end
   
 protected
   
