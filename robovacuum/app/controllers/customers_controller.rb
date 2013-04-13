b0d61281d76f82189df576d4fd9be0a4db84ea5c
Daniel Choi <zumerfolg@gmail.com>
2013-04-13 12:11:10 -0500

    organize files

--- robovacuum/app/controllers/customers_controller.rb
+++ robovacuum/app/controllers/customers_controller.rb
@@ -1,7 +1,7 @@
 class CustomersController < ApplicationController
   
   before_filter :get_cart, :only => [:new, :create]
-  before_filter :get_province,  :only => [:new, :edit, :create]
+  before_filter :get_province,  :only => [:new, :create]
   
   
   def get_province
@@ -9,28 +9,6 @@ class CustomersController < ApplicationController
   end  
   
   
-  # GET /customers
-  # GET /customers.json
-  def index
-    @customers = Customer.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @customers }
-    end
-  end
-
-  # GET /customers/1
-  # GET /customers/1.json
-  def show
-    @customer = Customer.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @customer }
-    end
-  end
-
   # GET /customers/new
   # GET /customers/new.json
   def new
@@ -40,11 +18,6 @@ class CustomersController < ApplicationController
       format.html # new.html.erb
       format.json { render json: @customer }
     end
-  end
-
-  # GET /customers/1/edit
-  def edit
-    @customer = Customer.find(params[:id])
   end
 
   # POST /customers
@@ -63,33 +36,6 @@ class CustomersController < ApplicationController
     end
   end
 
-  # PUT /customers/1
-  # PUT /customers/1.json
-  def update
-    @customer = Customer.find(params[:id])
-
-    respond_to do |format|
-      if @customer.update_attributes(params[:customer])
-        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /customers/1
-  # DELETE /customers/1.json
-  def destroy
-    @customer = Customer.find(params[:id])
-    @customer.destroy
-
-    respond_to do |format|
-      format.html { redirect_to customers_url }
-      format.json { head :no_content }
-    end
-  end
   
 protected
   
