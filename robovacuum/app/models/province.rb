2ae064cc3aaa1e1cb28486785dab845c07abae50
Daniel Choi <zumerfolg@gmail.com>
2013-04-13 23:54:31 -0500

    add numericality

--- robovacuum/app/models/province.rb
+++ robovacuum/app/models/province.rb
@@ -1,6 +1,9 @@
 class Province < ActiveRecord::Base
   attr_accessible :gst, :hst, :name, :pst
-  validates :gst, :hst, :name, :pst, :presence => true
+  
+  validates :name, :gst, :hst, :pst, :presence => true
+  validates :name, :uniqueness => true
+  validates :gst, :hst, :pst, :numericality => true
   
   has_many :customers
   
